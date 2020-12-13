83415e5f296e8b3cc6fcdb4665ea8a48c0183266
Julia Cordero <jrcorder@usc.edu>
2020-12-13 09:54:32 -0800

    edit form in modal

--- raindyday/src/pages/home/tabs/loans/index.js
+++ raindyday/src/pages/home/tabs/loans/index.js
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { Button, Modal, Form } from 'react-bootstrap'
+
 let example_data = [
     {
         name: 'Congress Bill',
@@ -31,20 +33,78 @@ function Pill(props) {
     )
 }
 
+function handleSubmit() {
+    /* Data is captured but not fully validated or parsed */
+    var workers = document.getElementById("numWorkers").value;
+    var location = document.getElementById("location").value;
+    var annualRevenue = document.getElementById("annualRevenue").value;
+}
+
+function EditModal() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <div>
+          <Button variant="primary" onClick={handleShow}>
+            Edit
+          </Button>
+
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Edit your information</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                  <Form.Group controlId="formNumWorkers">
+                    <Form.Label>Number of workers</Form.Label>
+                    <Form.Control placeholder="Enter number" type="number" id="numWorkers"/>
+                  </Form.Group>
+
+                  <Form.Group controlId="formLocation">
+                    <Form.Label>Location</Form.Label>
+                    <Form.Control placeholder="Enter location" id="location"/>
+                  </Form.Group>
+
+                  <Form.Group controlId="formAnnualRevenue">
+                    <Form.Label>Annual Revenue</Form.Label>
+                    <Form.Control placeholder="Enter annual revenue" type="number" id="annualRevenue"/>
+                  </Form.Group>
+                  <Button variant="primary" type="submit" onClick={handleSubmit}>
+                    Submit
+                  </Button>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
+      );
+}
+
 function Loans() {
     const [bills, setBills] = useState(example_data)
     return (
-        <>
-            <h4>Your Information</h4>
+        <div>
+            <div className="row">
+                <h4>Your Information</h4>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <EditModal></EditModal>
+            </div>
             <div className="row">
                 <Pill># of workers: {30}</Pill>
                 <Pill>Location: {'LA, CA'}</Pill>
-                <Pill>Revenue: {'look annually'}</Pill>
+                <Pill>Revenue: {'100k annually'}</Pill>
             </div>
             {bills.map((bill) => {
                 return <Bill name={bill.name} />
             })}
-        </>
+        </div>
     )
 }
 
