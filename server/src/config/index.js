c80bfd2364f0ce24cdf9a8ab9728ffcb7eca602b
Devin Mui <devinwmui@gmail.com>
2020-12-18 15:19:59 -0800

    translate pai

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -3,6 +3,7 @@ export default {
     NODE_ENV: process.env.NODE_ENV || 'development',
     finastra_client_id: process.env.finastra_client_id,
     finastra_client_secret: process.env.finastra_client_secret,
+    projectId: process.env.projectId, // google cloud project id
     base_url: process.env.base_url,
     token_path: process.env.token_path,
     authorize_path: process.env.authorize_path,
