929d30ebcbc86642916e8a883f421f11ec542c03
Devin Mui <devinwmui@gmail.com>
2020-12-18 16:17:46 -0800

    added google key to gitignore

--- server/src/routes/api/index.js
+++ server/src/routes/api/index.js
@@ -2,13 +2,13 @@ import express from 'express'
 import users from './users'
 import sheets from './sheets'
 import analyze from './analyze'
-// import translate from './translate'
+import translate from './translate'
 
 let router = express.Router()
 
 router.use('/users', users)
 router.use('/sheets', sheets)
 router.use('/analyze', analyze)
-// router.use('/translate', translate)
+router.use('/translate', translate)
 
 export default router
